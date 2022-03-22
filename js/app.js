31df8186a43e79f20d7c60c2ddb9f38fae4f558d
lilienamorado <94657576+lilienamorado@users.noreply.github.com>
2022-03-21 21:30:07 -0600

    examen 2 app

---
+++ js/app.js
@@ -0,0 +1,89 @@
+// Dom7
+var $$ = Dom7;
+
+// Init App
+var app = new Framework7({
+  	id: 'com.app',
+  	root: '#app',
+	theme: 'md',
+	view: {
+        pushState: true
+    },
+	cache:false,
+	cacheDuration: 0,
+	modalTitle: 'Movies Shop',
+  	routes: [
+		{
+		path: '/home/',
+    	url: 'index.html',
+    	name: 'home',
+  		},
+		{
+		path: '/contenido/',
+    	url: 'contenido.html',
+    	name: 'contenido',
+  		},
+		
+		{
+		path: '/bag/',
+    	url: 'bag.html',
+    	name: 'bag',
+  		},
+		
+	],
+	dialog: {
+		title: 'Movies Shop',
+		buttonOk: 'Aceptar',
+  	},
+	popup: {
+		closeOnEscape: true,
+		backdrop : false
+	},
+	sheet: {
+		closeOnEscape: true,
+	},
+	popover: {
+		closeOnEscape: true,
+	},
+	actions: {
+		closeOnEscape: true,
+	}
+});
+
+
+
+$$('#btnLogin').on('click', function (e) {
+	e.preventDefault();
+	
+	var $valid = $$('#form-login')[0].checkValidity();
+	if ($valid){
+        $$('#form-login').trigger('reset');	
+        app.loginScreen.close('#login');
+		
+		app.dialog.alert('¡ Welcome to Movies Shop !');
+    }
+	
+});
+
+
+
+$$(document).on('click', '#btnAddCart', function (e) {
+	e.preventDefault();
+
+	
+	var notification = app.notification.create({
+      icon: '<i class="material-icons">check</i>',
+      title: 'Order',
+      titleRightText: '',
+      subtitle: '',
+      text: "Your order has been received.",
+      closeTimeout: 3000,
+    });
+    notification.open();
+	
+});
+
+
+
+
+
